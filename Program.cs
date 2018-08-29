0a0e05181f69c4611a1521489adb9f32c8dba354
Aleksandar Tasev <40795644+AlexTasev@users.noreply.github.com>
2018-08-29 20:44:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_Problem
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = length * width * height;            // in cm
+
+            double volInL = volume / 1000.0;
+            double percSand = percent / 100.0;
+
+            double freeVol = volInL - (volInL * percSand);
+
+            Console.WriteLine($"{freeVol:f3}");
+        }
+    }
+}
